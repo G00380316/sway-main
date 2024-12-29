49b668c266b57cc869ea36bb5923d10fe9194cb7
Enoch Abidoun <ezea128@gmail.com>
2024-12-29 04:05:28 +0000

    fix dir

--- install.sh
+++ install.sh
@@ -23,11 +23,8 @@ fi
 echo "Git is installed. Continuing with the script..."
 # Add further commands here after ensuring Git is installed
 
-
-
 # Clone the repository into the home directory
-git clone https://github.com/G00380316/ArchDispManConf.git ~/sway
-
+git clone https://github.com/G00380316/ArchDispManConf.git
 
 clear
 echo "
@@ -42,32 +39,32 @@ echo "
 # chmod +x setup.sh packages.sh
 
 # Run the setup script
-zsh ~/sway/install_scripts/setup.sh
+zsh ~/ArchDispManConf/install_scripts/setup.sh
 
 clear
 
 # Run the extra packages
-zsh ~/sway/install_scripts/packages.sh
+zsh ~/ArchDispManConf/install_scripts/packages.sh
 
 clear
 
 echo "Make sure a Display Manager is installed"
 
 # make sure gdm3 is installed
-zsh ~/sway/install_scripts/gdm.sh
+zsh ~/ArchDispManConf/install_scripts/gdm.sh
 
 clear
 
 # add bashrc question
-zsh ~/sway/install_scripts/add_bashrc.sh
+zsh ~/ArchDispManConf/install_scripts/add_bashrc.sh
 
 clear
 
-zsh ~/sway/install_scripts/printers.sh
+zsh ~/ArchDispManConf/install_scripts/printers.sh
 
 clear
 
-zsh ~/sway/install_scripts/bluetooth.sh
+zsh ~/ArchDispManConf/install_scripts/bluetooth.sh
 sudo pacman -Rns $(pacman -Qdtq)
 yay -Rns $(yay -Qdtq)
 
