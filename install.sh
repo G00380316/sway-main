c90ea78c4e00c992f508ca4c894b6532be3856be
Enoch Abidoun <ezea128@gmail.com>
2024-12-29 03:59:42 +0000

    fix url

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Check if git is installed
 if ! command -v git &> /dev/null; then
     echo "Git is not installed. Attempting to install Git..."
-    
+
     # Use apt to install git
     if command -v apt &> /dev/null; then
         sudo pacman -Sy
@@ -12,7 +12,7 @@ if ! command -v git &> /dev/null; then
         echo "Cannot install Git automatically using pacman. Please install Git manually and run this script again."
         exit 1
     fi
-    
+
     # Check again if git is installed after attempting to install
     if ! command -v git &> /dev/null; then
         echo "Git installation failed. Please install Git manually and run this script again."
@@ -26,16 +26,16 @@ echo "Git is installed. Continuing with the script..."
 
 
 # Clone the repository into the home directory
-git clone https://github.com/G00380316/sway ~/sway
+git clone https://github.com/G00380316/ArchDispManConf.git ~/sway
 
 
 clear
 echo "
- +-+-+-+-+-+-+-+-+-+-+-+-+-+ 
+ +-+-+-+-+-+-+-+-+-+-+-+-+-+
  | | |G|0|0|3|8|0|3|1|6| | |
- +-+-+-+-+-+-+-+-+-+-+-+-+-+ 
- |c|u|s|t|o|m| |s|c|r|i|p|t| 
- +-+-+-+-+-+-+ +-+-+-+-+-+-+                                                                                                            
+ +-+-+-+-+-+-+-+-+-+-+-+-+-+
+ |c|u|s|t|o|m| |s|c|r|i|p|t|
+ +-+-+-+-+-+-+ +-+-+-+-+-+-+
 "
 
 # Make setup.sh executable (if needed, though it's typically already executable)
@@ -61,11 +61,11 @@ clear
 # add bashrc question
 zsh ~/sway/install_scripts/add_bashrc.sh
 
-clear 
+clear
 
 zsh ~/sway/install_scripts/printers.sh
 
-clear 
+clear
 
 zsh ~/sway/install_scripts/bluetooth.sh
 sudo pacman -Rns $(pacman -Qdtq)
